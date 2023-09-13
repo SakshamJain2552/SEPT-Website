c73d555cb4f3d792f78a4dbe8cb79bf876a78f9e
Laura Gatt <s3945256@student.rmit.edu.au>
2023-09-13 20:31:19 +1000

    App js and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './Components/NavBar';
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import ProfilePage from './Pages/ProfilePage';
+import CartPage from './Pages/CartPage';
+import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
+
 
 function App() {
   return (
+   <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/about' element={<AboutPage/>} />
+          <Route path='/profile' element={<ProfilePage/>} />
+          <Route path='/cart' element={<CartPage/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
