47a1b79beb3772ec60a271251414b40955b110ac
Laura Gatt <s3945256@student.rmit.edu.au>
2023-09-13 20:29:35 +1000

    Cart page file

---
+++ frontend/src/Pages/CartPage.js
@@ -0,0 +1,13 @@
+// CartPage.js
+import React from 'react';
+
+function CartPage() {
+  return (
+    <div>
+      <h1>Cart Page</h1>
+      {/* Add content for the home page */}
+    </div>
+  );
+}
+
+export default CartPage;
