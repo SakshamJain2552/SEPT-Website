b6032b399b6e6f91f6150e30f74becd5859b5eb7
Krisanahari <s3941378@student.rmit.edu.au>
2023-09-15 01:57:51 +1000

    Failure Test: Product Servicer empty function

---
+++ Backend/ProductService/src/main/java/ProductService/ProductService/Service/ProductServicerImpl.java
@@ -0,0 +1,26 @@
+package ProductService.ProductService.Service;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ProductService.ProductService.Model.Product;
+import ProductService.ProductService.Repository.ProductRepository;
+
+@Service
+public class ProductServicerImpl implements ProductServicer{
+    
+    private final ProductRepository repository;
+
+    @Autowired
+    public ProductServicerImpl(ProductRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public Collection<Product> getProducts(){
+        return null;
+    }
+
+}
