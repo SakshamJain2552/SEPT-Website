40a73ac9afa5f0abccd3cbc1e909743ce1f679a8
Laura Gatt <s3945256@student.rmit.edu.au>
2023-09-14 19:43:17 +1000

    Implemented footer page

---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { Box, Container, Grid, Link, Typography } from '@mui/material';
+
+const Footer = () => {
+  return (
+    <Box sx={{ bgcolor: 'white', color: 'primary.main', py: 4 }}>
+      <Container>
+        <Grid container spacing={3}>
+            <Grid item xs={12} sm={6} md={3}>
+                <Typography variant="h6" gutterBottom fontWeight="bold">
+                    About Us
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Aim
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Vision
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Testimonials
+                </Link>
+                </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+                <Typography variant="h6" gutterBottom fontWeight="bold">
+                    Services
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Writing
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Internships
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Coding
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Teaching
+                </Link>
+                </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+                <Typography variant="h6" gutterBottom fontWeight="bold">
+                Contact Us
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Laura
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Lawrence
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Saksham
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Hari
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    Darby
+                </Link>
+                </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+                <Typography variant="h6" gutterBottom fontWeight="bold">
+                Social Media
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    <i className="fab fa-facebook-f" />
+                    <span style={{ marginLeft: '10px' }}>Facebook</span>
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    <i className="fab fa-instagram" />
+                    <span style={{ marginLeft: '10px' }}>Instagram</span>
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    <i className="fab fa-twitter" />
+                    <span style={{ marginLeft: '10px' }}>Twitter</span>
+                </Link>
+                </Typography>
+                <Typography variant="subtitle1">
+                <Link href="#" color="primary">
+                    <i className="fab fa-youtube" />
+                    <span style={{ marginLeft: '10px' }}>Youtube</span>
+                </Link>
+                </Typography>
+            </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
