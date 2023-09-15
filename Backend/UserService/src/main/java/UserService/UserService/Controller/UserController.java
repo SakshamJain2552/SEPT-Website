1c3f9902035703d27ec61712fff4996efc2e8c7f
Lance Belen <s3944846@student.rmit.edu.au>
2023-09-15 21:03:34 +1000

    Added user-service base code and implemented sign-in feature

---
+++ Backend/UserService/src/main/java/UserService/UserService/Controller/UserController.java
@@ -0,0 +1,24 @@
+package UserService.UserService.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import UserService.UserService.Service.UserService;
+
+@RestController
+@RequestMapping(value = "user")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+    
+    @PostMapping
+    public boolean userLogin(String username, String password) {
+        return userService.userVerified(username, password);
+    } 
+}
---
+++ Backend/UserService/src/main/java/UserService/UserService/Repository/UserRepoImpl.java
@@ -0,0 +1,49 @@
+package UserService.UserService.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepoImpl implements UserRepo {
+    private final DataSource dataSource;
+
+    public UserRepoImpl(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public boolean userFound(String username, String password) {
+        
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement stm = connection.prepareStatement(
+                "SELECT UserID, Username, Password, Email\r\n" + //
+                "FROM Users;"
+            );
+
+            ResultSet rs = stm.executeQuery();
+
+            while(rs.next()) {
+                
+                if (rs.getString(2).equals(username) && rs.getString(3).equals(password)) {
+                    connection.close();
+                    return true;
+                }
+            }
+
+            connection.close();
+            return false;
+            
+        } catch (SQLException e) {
+            throw new RuntimeException("Error in findAll()", e);
+        }
+
+    }
+
+}
