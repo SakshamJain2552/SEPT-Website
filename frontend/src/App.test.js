d2edb2d14cf44725dce774ad3f1c86f3f995469d
SakshamJain2552 <s3923854@student.rmit.edu.au>
2023-09-16 03:24:29 +1000

    Added test for App.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,2 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
+
+test('renders the App component', () => {
+  render(<App />);
+});
