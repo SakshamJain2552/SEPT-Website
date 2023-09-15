f11faa193aeb87f3e7fbb34640002a71b1877f5c
SakshamJain2552 <s3923854@student.rmit.edu.au>
2023-09-16 03:00:26 +1000

    UI for Sign-In (no functional backend yet)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/signin'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#C4E15B',  // e.g., '#ff5722'
+    },
+    secondary: {
+      main: '#50C878',   // e.g., '#f44336'
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Routes>
+          {/* <Route path="/" element={<SignIn />}>
+          <Route index element={<BookList />} />
+          <Route path="list" element={<BookList />} />
+        </Route> */}
+
+          <Route path="/" element={<SignIn />} />
+
+      </Routes>
+      </BrowserRouter>
+      </ThemeProvider>
   );
 }
 
---
+++ frontend/src/pages/signin.js
@@ -0,0 +1,129 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
+import logo from './logo.png';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+        SuperPrice
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+
+    navigate("/list");
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="md"> {/* Increased maxWidth to "md" */}
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h4" sx={{ m: 2 }}> {/* Replaced Avatar with Typography */}
+            {/* SuperPrice */}
+            <img src={logo} alt="Logo" />
+          </Typography>
+          <Typography component="h2" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="secondary" />}
+              label="Remember me"
+            />
+            <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+            <Button
+  type="submit"
+  fullWidth
+  variant="contained"
+  color="success"
+  sx={{
+    mt: 3,
+    mb: 2,
+    bgcolor: '#C4E15B',
+    borderRadius: '20px',
+    height: '50px',
+    width: '500px',
+    color: '#7F808F', // Text color
+    fontWeight: 'bold', // Make the text bold
+  }}
+>
+  Sign In
+</Button>
+
+              
+              </div>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
