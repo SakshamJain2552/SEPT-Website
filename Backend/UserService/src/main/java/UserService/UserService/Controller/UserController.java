11447e72bcd61d159ce754d6ed622dee04a50068
SakshamJain2552 <s3923854@student.rmit.edu.au>
2023-09-17 17:08:02 +1000

    Added CrossOrigin to UserController

--- Backend/UserService/src/main/java/UserService/UserService/Controller/UserController.java
+++ Backend/UserService/src/main/java/UserService/UserService/Controller/UserController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import UserService.UserService.Service.UserService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/user")
 public class UserController {
     private final UserService userService;
