53056acec1b3c7d7c34f81e271abee7b7ccaa7a9
Laura Gatt <s3945256@student.rmit.edu.au>
2023-09-17 17:29:33 +1000

    extra fixes and deletes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
 import NavBar from './Components/NavBar';
-import HomePage from './Pages/HomePage';
-import AboutPage from './Pages/AboutPage';
-import ProfilePage from './Pages/ProfilePage';
-import CartPage from './Pages/CartPage';
-import Category1 from './Pages/Category1';
-import Category2 from './Pages/Category2';
-import Category3 from './Pages/Category3';
+import HomePage from './Pages/IconPages/HomePage';
+import LikedProducts from './Pages/IconPages/LikedProducts';
+import ProfilePage from './Pages/IconPages/ProfilePage';
+import CartPage from './Pages/IconPages/CartPage';
+import LowestPrices from './Pages/ShopPages/LowestPrices';
+import AllStores from './Pages/StoresPages/AllStores';
+import CompareStores from './Pages/StoresPages/CompareStores';
 import Footer from './Components/Footer';
+import AllProducts from './Pages/ShopPages/AllProducts';
+import AboutAccount from './Pages/AccountPages/AboutAccount';
+import AccountNotifications from './Pages/AccountPages/AccountNotifications';
+import AboutLaura from './Pages/FooterPages/AboutLaura';
+import AboutLawrence from './Pages/FooterPages/AboutLawrence';
+import AboutSaksham from './Pages/FooterPages/AboutSaksham';
+import AboutHari from './Pages/FooterPages/AboutHari';
+import AboutLance from './Pages/FooterPages/AboutLance';
+import AboutDarby from './Pages/FooterPages/AboutDarby';
 
 
 function App() {
@@ -19,14 +28,23 @@ function App() {
         <NavBar />
           <Routes>
             <Route path='/' element={<HomePage/>} />
-            <Route path='/about' element={<AboutPage/>} />
+            <Route path='/liked-products' element={<LikedProducts/>} />
             <Route path='/profile' element={<ProfilePage/>} />
             <Route path='/cart' element={<CartPage/>} />
-            <Route path='/category1' element={<Category1/>} />
-            <Route path='/category2' element={<Category2/>} />
-            <Route path='/category3' element={<Category3/>} />
+            <Route path='/shop/all-products' element={<AllProducts/>} />
+            <Route path='/shop/lowest-prices' element={<LowestPrices/>} />
+            <Route path='/stores/all-stores' element={<AllStores/>} />
+            <Route path='/stores/compare-stores' element={<CompareStores/>} />
+            <Route path='/account/about' element={<AboutAccount/>} />
+            <Route path='/account/notifications' element={<AccountNotifications/>} />
+            <Route path='/contact/laura' element={<AboutLaura/>} />
+            <Route path='/contact/lawrence' element={<AboutLawrence/>} />
+            <Route path='/contact/saksham' element={<AboutSaksham/>} />
+            <Route path='/contact/hari' element={<AboutHari/>} />
+            <Route path='/contact/lance' element={<AboutLance/>} />
+            <Route path='/contact/darby' element={<AboutDarby/>} />
           </Routes>
-        <Footer />  
+        <Footer /> 
       </div>
     </Router>
   );
