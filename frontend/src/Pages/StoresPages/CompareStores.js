15c86fb2bc8c17a465d8a2fe74b66468e823166e
Laura Gatt <s3945256@student.rmit.edu.au>
2023-09-17 17:28:31 +1000

    Added more pages

---
+++ frontend/src/Pages/StoresPages/CompareStores.js
@@ -0,0 +1,12 @@
+// CompareStores.js
+import React from 'react';
+
+function CompareStores() {
+  return (
+    <div>
+      <h1>Compare Stores</h1>
+    </div>
+  );
+}
+
+export default CompareStores;
