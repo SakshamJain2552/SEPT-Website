4f6ad565aaac60ae339d5d9a613fc16b5e050357
SakshamJain2552 <s3923854@student.rmit.edu.au>
2023-09-17 18:34:57 +1000

    Attempted CI Fix for Jest: Axios

--- frontend/src/pages/display-products.js
+++ frontend/src/pages/display-products.js
@@ -1,64 +1,12 @@
 import React from 'react';
 import { Grid, Container, Breadcrumbs, Link, Typography, Box } from '@mui/material';
-import Sidebar from '../components/sidebar.js'; // Import your Sidebar, FilterBar, and BookList components
+import Sidebar from '../Components/sidebar.js'; // Import your Sidebar, FilterBar, and BookList components
 // import FilterBar from './FilterBar';
-import Productlist from '../components/productlist.js';
-
-// function AllProducts() {
-//   return (
-//         <Box display="flex">
-//           <Sidebar />
-//           <Box display="flex" flexDirection="column" flex={1} style={{marginRight: '100px' }}>
-//             {/* <FilterBar /> */}
-//             <Productlist />
-//           </Box>
-//         </Box>
-//   );
-// }
-
-// function AllProducts() {
-//     return (
-//           <Box display="flex" style={{ width: '100%', minHeight: '100vh', overflowY: 'auto' }}>
-//             <Sidebar />
-//             <Box display="flex" flexDirection="column" flex={1} style={{ marginLeft: '20px', marginRight: '20px' }}>
-//               <Productlist />
-//             </Box>
-//           </Box>
-//     );
-//   }
-
-// function AllProducts() {
-//     return (
-//         //   <Box display="flex" style={{ width: '100%', minHeight: '100vh', overflowY: 'scroll' }}>
-//         //     <Sidebar />
-//         //     <Box display="flex" flexDirection="column" flex={1} style={{ marginLeft: '150px', marginRight: '100px', minHeight: '100vh' }}>
-//               <Productlist />
-//             /* </Box>
-//           </Box> */
-//     );
-//   }
-  
+import Productlist from '../Components/productlist';
 
 // export default AllProducts;
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-
-// function MainPage() {
-//   return (
-//     <div>
-//       <NavBar />
-//       <div>
-//         {/* <Sidebar /> */}
-//         <Productlist />
-//           </div>
-//           <div>
-//               <NavBar />
-//           </div>
-//     </div>
-//   );
-// }
-
-// export default MainPage;
+import NavBar from '../Components/NavBar';
+import Footer from '../Components/Footer';
 
 function MainPage() {
     return (
