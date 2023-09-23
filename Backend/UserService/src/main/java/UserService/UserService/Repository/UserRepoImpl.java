66fd09a28f2b5b15e7e50c64cceb24e06a1fbe87
Krisanahari <s3941378@student.rmit.edu.au>
2023-09-23 22:45:28 +1000

    Pass Test: Implemented - read cart - repo

--- Backend/UserService/src/main/java/UserService/UserService/Repository/UserRepoImpl.java
+++ Backend/UserService/src/main/java/UserService/UserService/Repository/UserRepoImpl.java
@@ -1,6 +1,7 @@
 package UserService.UserService.Repository;
 
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.sql.Connection;
@@ -216,7 +217,38 @@ public class UserRepoImpl implements UserRepo {
 
     @Override
     public List<Cart> findById(Long id) {
-        return null;
+        List<Cart> cartItems = new ArrayList<>();
+
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(
+                "SELECT CI.CartItemID, CI.CartID, CI.ProductID, CI.StoreName, CI.Quantity, C.UserID, C.DateCreated " +
+                "FROM CartItems CI " +
+                "JOIN Carts C ON CI.CartID = C.CartID " +
+                "WHERE CI.CartID = ?;"
+            );
+            stmt.setLong(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Cart cart = new Cart();
+                cart.cartItemId = rs.getLong("CartItemID");
+                cart.cartId = rs.getLong("CartID");
+                cart.productId = rs.getLong("ProductID");
+                cart.storeName = rs.getString("StoreName");
+                cart.quantity = rs.getLong("Quantity");
+                cart.userId = rs.getLong("UserID");
+                cart.dateCreated = rs.getString("DateCreated");
+
+                cartItems.add(cart);
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error in findById", e);
+        }
+
+        return cartItems;
     }
 
 }
