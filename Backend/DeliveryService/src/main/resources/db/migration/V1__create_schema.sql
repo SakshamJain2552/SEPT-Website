bff04fc8eefe6086693c0e5a1964c52be65036ab
Lance Belen <s3944846@student.rmit.edu.au>
2023-09-26 20:49:28 +1000

    Added scripts to create table for delivery service and insert sample data

--- Backend/DeliveryService/src/main/resources/db/migration/V1__create_schema.sql
+++ Backend/DeliveryService/src/main/resources/db/migration/V1__create_schema.sql
@@ -1,1 +1,30 @@
--- wait for the db lol
+-- H2 SQL Script for Flyway
+-- Compatible with MySQL and H2
+
+-- Use AUTO_INCREMENT for H2
+-- MySQL uses AUTO_INCREMENT, while H2 uses IDENTITY
+
+-- -----------------------------------------------------
+-- Table Users
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Users (
+  UserID INT AUTO_INCREMENT PRIMARY KEY,
+  Username VARCHAR(255) NOT NULL,
+  Password VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) NOT NULL,
+  CONSTRAINT UQ_Username UNIQUE (Username),
+  CONSTRAINT UQ_Email UNIQUE (Email)
+);
+
+-- -----------------------------------------------------
+-- Table OrderDetails
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS OrderDetails (
+    OrderID INT AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(255) NOT NULL,
+    DeliveryAddress VARCHAR(255) NOT NULL,
+    DeliveryDate VARCHAR(255) NOT NULL,
+    DeliveryTime VARCHAR(255) NOT NULL,
+    PaymentMethod VARCHAR(255) NOT NULL,
+    CONSTRAINT FK_Username_Order FOREIGN KEY (Username) REFERENCES Users(Username)
+)
--- Backend/DeliveryService/src/main/resources/db/migration/V2__insert_schema.sql
+++ Backend/DeliveryService/src/main/resources/db/migration/V2__insert_schema.sql
@@ -1,1 +1,6 @@
--- wait for db lol
+-- Sample Inputs for Users
+INSERT INTO OrderDetails (Username, DeliveryAddress, DeliveryDate, DeliveryTime, PaymentMethod) VALUES
+('alice123', '1 Alice Street, Victoria', '01-01-2023', '12:00', 'card'),
+('bob456', '2 Bob Street, Victoria', '01-01-2023', '15:00', 'cash'),
+('charlie789', '3 Charlie Street, Victoria', '01-01-2023', '09:00', 'card'),
+('david101', '4 David Street, Victoria', '01-01-2023', '10:00', 'cash');
