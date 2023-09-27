628cebe1c4a477b15be0dd1c09c641378aa35eaa
Krisanahari <s3941378@student.rmit.edu.au>
2023-09-27 17:57:07 +1000

    Created user review - service

--- Backend/ProductService/src/main/java/ProductService/ProductService/Service/ProductServicer.java
+++ Backend/ProductService/src/main/java/ProductService/ProductService/Service/ProductServicer.java
@@ -5,11 +5,16 @@ import java.util.Optional;
 
 import ProductService.ProductService.Model.DetailedProduct;
 import ProductService.ProductService.Model.Product;
+import ProductService.ProductService.Model.UserReview;
 
 public interface ProductServicer {
     
     public Collection<Product> getProducts();
 
     public Optional<DetailedProduct> findDetailProduct(Long id);
+
+    public UserReview setUserReview(UserReview userReview);
+
+    public Double getUserReview(Long productId);
 
 }
