eb66434f9bbbea49dbfcc0ae079db0d804f6c8cc
Laura Gatt <s3945256@student.rmit.edu.au>
2023-09-27 19:07:52 +1000

    more profile page stuff

--- frontend/src/pages/IconPages/ProfilePage.js
+++ frontend/src/pages/IconPages/ProfilePage.js
@@ -1,37 +1,97 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import { useParams } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-export default function UserDetails() {
-  const { userId } = useParams(); // Assuming you have a route parameter for the user ID
-  const [userDetails, setUserDetails] = useState(null);
+const headingStyle = {
+  fontSize: '24px', // Adjust the font size as needed
+  fontWeight: 'bold', // Make the text bold
+  marginBottom: '20px', // Add some spacing below the heading
+};
 
-  // Simulating fetching user details from the backend
+function UserDetails() {
+  const [userDetails, setUserDetails] = useState({});
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    // Replace this with actual API request to fetch user details by ID
-    // Example fetch:
-    fetch(`http://example.com/api/users/${userId}`)
-      .then((response) => response.json())
-      .then((data) => setUserDetails(data))
-      .catch((error) => console.error('Error fetching user details:', error));
-  }, [userId]);
+    // Replace 'currentUsername' with the actual username of the current user
+    const currentUsername = 'exampleUsername'; // You should get this from your authentication system
+
+    axios.get(`http://localhost:8080/user/details?username=${currentUsername}`)
+      .then((response) => {
+        setUserDetails(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching user details:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <Container maxWidth="md">
-      <Typography variant="h4" gutterBottom>
-        User Details
-      </Typography>
-      {userDetails ? (
-        <div>
-          <Typography variant="h6">User ID: {userDetails.id}</Typography>
-          <Typography>Name: {userDetails.name}</Typography>
-          <Typography>Email: {userDetails.email}</Typography>
-          {/* Add more user details as needed */}
-        </div>
-      ) : (
-        <Typography>Loading user details...</Typography>
-      )}
+    <Container component="main" maxWidth="xs">
+      <div>
+        <Typography variant="h5" style={headingStyle}>
+          User Details
+        </Typography>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          label="Username"
+          value={userDetails.username}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+         <TextField
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          label="Name"
+          value={userDetails.name}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+         <TextField
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          label="Last name"
+          value={userDetails.lastname}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+         <TextField
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          label="Password"
+          value={userDetails.password}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        {/* Add more user details fields */}
+        <Button
+          type="button"
+          variant="contained"
+          color="primary"
+          fullWidth
+        >
+          Edit Details
+        </Button>
+      </div>
     </Container>
   );
 }
+
+export default UserDetails;
