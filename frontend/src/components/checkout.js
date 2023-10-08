c61f21434538356e37032d9fa6fd5c334b011c0c
SakshamJain2552 <s3923854@student.rmit.edu.au>
2023-10-08 18:17:24 +1100

    Added Button to view all Orders

--- frontend/src/components/checkout.js
+++ frontend/src/components/checkout.js
@@ -212,33 +212,6 @@ const [responseMessage, setResponseMessage] = useState('');
             Confirm Order
       </Button>
       
-      {/* <Modal
-            open={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-        >
-            <Box
-                style={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: 400,
-                    bgcolor: 'background.paper',
-                    border: '2px solid #000',
-                    boxShadow: 24,
-                    p: 4,
-                    textAlign: 'center'
-                }}
-            >
-                <CheckCircleOutlineIcon style={{ color: 'green', fontSize: 60, animation: 'spin 1s linear infinite' }} />
-                <Typography variant="h6" id="modal-title">
-                    {responseMessage}
-                </Typography>
-            </Box>
-        </Modal>
-         */}
       
       <Modal
     open={isModalOpen}
