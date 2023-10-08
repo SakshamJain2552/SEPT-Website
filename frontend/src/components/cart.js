8145afb0f970a3df357af7d45d248e2ce6ea72d1
SakshamJain2552 <s3923854@student.rmit.edu.au>
2023-10-08 18:14:32 +1100

    Added new API_URLs

--- frontend/src/components/cart.js
+++ frontend/src/components/cart.js
@@ -1,97 +1,10 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import DeleteIcon from '@mui/icons-material/Delete';
-// import Button from '@mui/material/Button';
-// import { useNavigate } from 'react-router-dom';
-
-// function CartPage() {
-//   const [cartItems, setCartItems] = useState([]);
-//   const [products, setProducts] = useState([]);
-//   const navigate = useNavigate(); // for redirecting
-
-//   useEffect(() => {
-//     fetchCartAndProducts();
-//   }, []);
-
-//   async function fetchCartAndProducts() {
-//     const userId = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user')).UserID;
-//     if (userId) {
-//       try {
-//         const cartResponse = await axios.get(`http://localhost:8080/user/cart/${userId}`);
-//         const productResponse = await axios.get('http://localhost:8081/products');
-        
-//         setCartItems(cartResponse.data);
-//         setProducts(productResponse.data);
-//       } catch (error) {
-//         console.error('Error fetching cart items and products', error);
-//       }
-//     }
-//   }
-
-//   const handleDelete = async (cartItem) => {
-//     const userId = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user')).UserID;
-    
-//     try {
-//       await axios.delete(`http://localhost:8080/user/cart`, {
-//         data: {
-//           userId: cartItem.userId,
-//           cartId: cartItem.cartId,
-//           cartItemId: cartItem.cartItemId
-//         }
-//       });
-
-//       // Update the cart items after successful deletion
-//       fetchCartAndProducts();
-//     } catch (error) {
-//       console.error('Error deleting cart item', error);
-//     }
-//   };
-
-//   return (
-//     <div style={{ maxWidth: '1200px', margin: '20px auto', padding: '20px' }}>
-//       <h2>Your Cart</h2>
-//       {cartItems.length === 0 ? (
-//         <p>Your cart is empty.</p>
-//       ) : (
-//         <div>
-//           {cartItems.map(item => {
-//             const product = products.find(p => p.productId === item.productId);
-//             return product ? (
-//               <div key={item.cartItemId} style={{ display: 'flex', borderBottom: '1px solid #eee', padding: '15px 0' }}>
-//                 <img src={product.imagePath} alt={product.productName} style={{ width: '100px', height: '100px', marginRight: '20px' }} />
-//                 <div style={{ flex: '1' }}>
-//                   <h4>{product.productName}</h4>
-//                   <p>Store: {item.storeName}</p>
-//                   <p>Price: ${product.price}</p>
-//                   <p>Quantity: {item.quantity}</p>
-//                   <p>Date Added: {item.dateCreated}</p>
-//                 </div>
-//                 <div onClick={() => handleDelete(item)} style={{ cursor: 'pointer' }}>
-//                   <DeleteIcon />
-//                 </div>
-//               </div>
-//             ) : null;
-//           })}
-//           {/* Checkout Button */}
-//           <div style={{ textAlign: 'right', marginTop: '20px' }}>
-//             <Button variant="contained" color="primary" onClick={() => navigate('/checkout')}>
-//               Checkout
-//             </Button>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default CartPage;
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import Breadcrumb from './Breadcrumbs';
+import { API_URL_1, API_URL_2, API_URL_3 } from './apiConfig';
 
 function CartPage() {
   const [cartItems, setCartItems] = useState([]);
@@ -105,11 +18,11 @@ function CartPage() {
     const userId = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user')).UserID;
     if (userId) {
       try {
-        const cartResponse = await axios.get(`http://localhost:8080/user/cart/${userId}`);
-        const productResponse = await axios.get('http://localhost:8081/products');
+        const cartResponse = await axios.get(`${API_URL_1}/user/cart/${userId}`);
+        const productResponse = await axios.get(`${API_URL_2}/products`);
         
         const detailedPrices = await Promise.all(cartResponse.data.map(async item => {
-          const productDetails = await axios.get(`http://localhost:8081/products/${item.productId}`);
+          const productDetails = await axios.get(`${API_URL_2}/products/${item.productId}`);
           const storeIndex = productDetails.data.storeNames.indexOf(item.storeName);
           return {
             productId: item.productId,
@@ -145,7 +58,7 @@ function CartPage() {
     const userId = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user')).UserID;
       
     try {
-      await axios.delete(`http://localhost:8080/user/cart`, {
+      await axios.delete(`${API_URL_1}/user/cart`, {
         data: {
           userId: cartItem.userId,
           cartId: cartItem.cartId,
